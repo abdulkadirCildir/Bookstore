e8ef88fc2fb5cd653f2ba5f3f5441ddeb8b42fb2
abdulkadirCildir <a.cildir89@gmail.com>
2020-12-13 17:41:09 +0100

    Add React project and Heroku

--- server.js
+++ server.js
@@ -13,6 +13,14 @@ app.use(express.json());                // Postman tarafindan gönderilen datayi
 app.use("/api", router);             // api router a gönderiyor
 
 
+// Production
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));  // heroku'nun client/build i kullanabilmesi icin..
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    });
+}
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
     console.log(`I'm listening on port ${port}`);
